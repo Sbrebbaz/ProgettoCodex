226800ccd66672189b82652384de9432204418ea
Davide Nembrini <davide.nembrini00@gmail.com>
2024-04-06 11:33:45 +0200

    initial commit
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>

---
+++ src/codexNaturalis/Main.java
@@ -0,0 +1,10 @@
+package codexNaturalis;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("TEST");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CodexNaturalis_45 {
+}
