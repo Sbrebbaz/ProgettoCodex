a2856ebbf992b7a7d724ebb91852e0de82a44935
Davide Nembrini <davide.nembrini00@gmail.com>
2024-04-20 19:11:48 +0200

    Created base main menu form
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -3,10 +3,22 @@ package ui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class MainMenu {
+	
+	private boolean startGame = false;
+	
+	public boolean isStartGame() {
+		return startGame;
+	}
 
-	protected Shell shell;
+	protected Shell shlCodex;
 
 	/**
 	 * Launch the application.
@@ -20,16 +32,16 @@ public class MainMenu {
 			e.printStackTrace();
 		}
 	}
-
+	
 	/**
 	 * Open the window.
 	 */
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlCodex.open();
+		shlCodex.layout();
+		while (!shlCodex.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -40,10 +52,46 @@ public class MainMenu {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlCodex = new Shell();
+		shlCodex.setSize(937, 581);
+		shlCodex.setText("Codex Naturalis");
+		FillLayout fl_shlCodex = new FillLayout(SWT.VERTICAL);
+		fl_shlCodex.spacing = 50;
+		fl_shlCodex.marginHeight = 100;
+		fl_shlCodex.marginWidth = 200;
+		shlCodex.setLayout(fl_shlCodex);
+		
+		
+		Label TitleLabel = new Label(shlCodex, SWT.CENTER);
+		TitleLabel.setAlignment(SWT.CENTER);
+		TitleLabel.setText("Codex Naturalis");
+		
+		Composite composite = new Composite(shlCodex, SWT.NONE);
+		FillLayout fl_composite = new FillLayout(SWT.VERTICAL);
+		fl_composite.spacing = 10;
+		composite.setLayout(fl_composite);
+		
+		Button StartGameButton = new Button(composite, SWT.NONE);
+		StartGameButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				//Set start game to true and close the form		
+				startGame = true;
+				shlCodex.close();				
+			}
+		});
+		StartGameButton.setText("START GAME");
+		
+		Button QuitGameButton = new Button(composite, SWT.NONE);
+		QuitGameButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				//Set start game to false and close the form
+				startGame = false;
+				shlCodex.close();	
+			}
+		});
+		QuitGameButton.setText("QUIT");
 
 	}
-
 }
