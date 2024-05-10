9e63a7b2ab4f6402c4fafe8a992ee17b543c5a96
uortiz <uortiz@sorint.it>
2024-05-10 18:48:31 +0200

    remove unused codes

--- src/base/Main.java
+++ src/base/Main.java
@@ -1,32 +1,9 @@
 package base;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // TODO Auto-generated method stub
-        List<Card> cards = new ArrayList<Card>();
-        Corner[] c = new Corner[2];
-        List<Corner> cl = new ArrayList<Corner>();
-        cl.add(new Corner(Symbol.ANIMALS));
-        cl.forEach(System.out::println);
-
-        Side s = new GenericSide(Collections.singletonList(Symbol.ANIMALS), c);
-        cards.add(new Card(Symbol.ANIMALS, s, s, CardType.STARTING) {
-        });
-
-        cards.forEach(System.out::println);
-
-        List<Symbol> symbols = new ArrayList<>();
-        List<PlaceRequirementSide> placeRequirements = new ArrayList<>();
-        placeRequirements.add(new PlaceRequirementSide(symbols, c, symbols));
-
-        placeRequirements.forEach(System.out::println);
-
-        Map m = new Map();
-        System.out.print(m.toString());
+        System.out.print("TEST");
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,5 @@
  */
 module CodexNaturalis_45 {
 	requires java.desktop;
+    requires org.junit.jupiter.api;
 }
