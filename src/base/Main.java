2cd4503687062335aeda93153cea9fdbd015dc29
Dantillio <116441723+Dantillio@users.noreply.github.com>
2024-05-17 21:09:28 +0200

    Renamed INSECT -> to INSECTS Updated main load card method Created CardJsonAdapter class
    Read and initialized all cards from JSON
    Removed CardUtility.java
    
    Signed-off-by: Davide Nembrini <davide.nembrini00@gmail.com>
    Co-authored-by: Davide Fiorentini <davide.fiorentini04@gmail.com>

--- src/base/Main.java
+++ src/base/Main.java
@@ -18,6 +18,7 @@ import java.util.Scanner;
 
 import base.*;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 import com.google.gson.internal.LinkedTreeMap;
@@ -30,52 +31,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 				
-		/*ArrayList<Symbol> symb = new ArrayList<Symbol>();
-		symb.add(Symbol.INK);
-		symb.add(Symbol.SCROLL);
-		
-		Side s = new GenericSide(
-				symb,
-				new Corner[] {new Corner(Symbol.INK),new Corner(Symbol.INK),new Corner(Symbol.INK),new Corner(Symbol.INK)});
-
-		Card c2 = new Card(Symbol.FEATHER,s,s,CardType.GOLD);
-		Card c3 = new Card(Symbol.FEATHER,s,s,CardType.GOLD);
-		
-		ArrayList<Card> cards = new ArrayList<Card>();
-		cards.add(c2);
-		cards.add(c3);
-		
-		Gson gson = new Gson();
-		
-	    try {
-	        FileWriter myWriter = new FileWriter("filename.txt");
-	        myWriter.write(gson.toJson(cards));
-	        myWriter.close();
-	        System.out.println("Successfully wrote to the file.");
-	      } catch (IOException e) {
-	        System.out.println("An error occurred.");
-	        e.printStackTrace();
-	      }*/
-		/*String data;
-		try {
-		      File myObj = new File("cards.json");
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		        data = myReader.nextLine();
-		        System.out.println(data);
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		JsonParser jsonParser = new JsonParser();
-		JsonArray jsonArray = (JsonArray) jsonParser.parse(data);*/
 		_readCardsFromFile();
 		ArrayList<Card> tmp = new ArrayList<Card>(Arrays.asList(cards));
-		//tmp.putAll(cards.get(0));
-		System.out.println(tmp);
 		
+		System.out.println(tmp);		
 	}
 	
 	private static void _readCardsFromFile() {
@@ -86,6 +45,9 @@ public class Main {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		cards = gson.fromJson(reader, Card[].class);
+		GsonBuilder gsonBldr = new GsonBuilder();
+	    gsonBldr.registerTypeAdapter(Card[].class, new CardJsonAdapter());
+	    
+	    cards = gsonBldr.create().fromJson(reader, Card[].class);
 	}
 }
