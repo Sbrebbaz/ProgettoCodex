6ac4f01bf88ecf35de5b1029e558be7e15a4da88
Dantillio <116441723+Dantillio@users.noreply.github.com>
2024-05-25 10:36:25 +0200

    bug fix table and package creation
    risolto bug di Table nella UIUtility.drawTablePhase, ordinati i file nei vari pacchetti

---
+++ card_components/PointCornerRequirementSide.java
@@ -0,0 +1,36 @@
+package card_components;
+
+import java.util.List;
+
+import enumeration.Symbol;
+import game_components.Map;
+
+public class PointCornerRequirementSide extends PlaceRequirementSide {
+
+    private PointCornerRequirementSide pointCornerRequirementSide;
+    public PointCornerRequirementSide(List<Symbol> center, Corner[] corners,int pointValue, List<Symbol> placeRequirements) {
+        super(center, corners,pointValue, placeRequirements);
+        // TODO Auto-generated constructor stub
+    }
+    public PointCornerRequirementSide(List<Symbol> center, Corner cornerTopLeft, Corner cornerTopRight,
+                                      Corner cornerBottomLeft, Corner cornerBottomRight,int pointValue, List<Symbol> placeRequirements) {
+        super(center, cornerTopLeft, cornerTopRight, cornerBottomLeft, cornerBottomRight,pointValue, placeRequirements);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int getPoints(Map map, int x, int y) {
+        return this.getPointValue() * map.getNearCorner(x, y);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(super.toString());
+        
+        sb.append("[Corner Requirement]");
+        
+        return sb.toString();
+    }
+}
---
+++ card_components/PointPositionRequirementSide.java
@@ -0,0 +1,82 @@
+package card_components;
+
+import java.util.Arrays;
+import java.util.List;
+
+import enumeration.Symbol;
+import game_components.Map;
+import game_components.MapCard;
+
+public class PointPositionRequirementSide extends PlaceRequirementSide {
+
+	private MapCard[][] positionRequirement;
+	/**
+	 * creates a PlaceRequirementSide from the center, corners, point value, a List of Symbol required to place the side and 
+	 * the subMap need to score points
+	 * @param center
+	 * @param corners
+	 * @param pointValue
+	 * @param placeRequirements
+	 * @param positionRequirement
+	 */
+	public PointPositionRequirementSide(List<Symbol> center, Corner[] corners,int pointValue, List<Symbol> placeRequirements,
+			MapCard[][] positionRequirement) {
+		super(center, corners,pointValue, placeRequirements);
+		this.positionRequirement = positionRequirement;
+	}
+	/**
+	 * creates a PlaceRequirementSide from the center, corners, point value, a List of Symbol required to place the side and 
+	 * the subMap need to score points
+	 * @param center
+	 * @param cornerTopLeft
+	 * @param cornerTopRight
+	 * @param cornerBottomLeft
+	 * @param cornerBottomRight
+	 * @param pointValue
+	 * @param placeRequirements
+	 * @param positionRequirement
+	 */
+	public PointPositionRequirementSide(List<Symbol> center, Corner cornerTopLeft, Corner cornerTopRight,
+			Corner cornerBottomLeft, Corner cornerBottomRight,int pointValue, List<Symbol> placeRequirements,
+			MapCard[][] positionRequirement) {
+		super(center, cornerTopLeft, cornerTopRight, cornerBottomLeft, cornerBottomRight,pointValue, placeRequirements);
+		this.positionRequirement = positionRequirement;
+	}
+
+	public MapCard[][] getPositionRequirement() {
+		return positionRequirement;
+	}
+
+	public void setPositionRequirement(MapCard[][] positionRequirement) {
+		this.positionRequirement = positionRequirement;
+	}
+
+	@Override
+	public int getPoints(Map map,int x,int y){
+		return map.findSubMap(positionRequirement)*this.getPointValue();
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(super.toString());
+		
+		for(int i=0;i<positionRequirement.length;i++) {
+			sb.append("\n\t");
+			for(int j=0;j<positionRequirement[0].length;j++) {
+				if(positionRequirement[i][j] != null) {
+					sb.append(positionRequirement[i][j].getCard().getCardColor().toString());
+				}
+				else {
+					sb.append("\t");
+				}
+			}
+		}
+		
+		sb.append("");
+		
+		return sb.toString();
+	}
+}
